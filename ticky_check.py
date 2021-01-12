e0f1867d92c4c6e41aa53ce67cc3cb4a576c2e0a
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-12 18:18:53 -0500

    Sorting done: ERRORS sorted by value, others by username

--- ticky_check.py
+++ ticky_check.py
@@ -6,10 +6,12 @@ import re
 import operator
 import csv
 
+# Dict: Count number of entries for each user
+per_user_info = {}  # Splitting between INFO and ERROR
+per_user_error = {}  # Splitting between INFO and ERROR
+
 # Dict: Number of different error messages
 errors = {}
-# Dict: Count number of entries for each user
-per_user = {}  # Splitting between INFO and ERROR
 
 # Sample Error
 # "May 27 11:45:40 ubuntu.local ticky: INFO: Created ticket [#1234] (username)"
@@ -19,15 +21,26 @@ with open('small.log') as file:
     for line in file.readlines():
         # regex search
         info = re.search(r'INFO: ([\w].*)', line)
+        user = re.search(r'\(.*?\)', line)
         error = re.search(r'ERROR: ([\w].*)', line)
 
         # Checks for successful match
         if info is not None:
-            e = info.group(0)
-            if not e in per_user:
-                per_user[e] = 1
+            i = info.group(0)
+            u = user.group(0)
+            if not u in per_user_info:
+                per_user_info[u] = 1
             else:
-                per_user[e] += 1
+                per_user_info[u] += 1
+
+        # Checks for ERROR match
+        if error is not None:
+            e = error.group(0)
+            u = user.group(0)
+            if not u in per_user_error:
+                per_user_error[u] = 1
+            else:
+                per_user_error[u] += 1
 
         # Checks if error got a match
         if error is not None:
@@ -39,10 +52,10 @@ with open('small.log') as file:
 file.close()
 
 
-# Sorted by Value (Most common to least common)
+# Sorted by VALUE (Most common to least common)
 print('ERRORS:', sorted(errors.items(),
                         key=operator.itemgetter(1), reverse=True))
 
-# Sorted by username
-print('Per User Dict:', sorted(per_user.items(),
-                               key=operator.itemgetter(1), reverse=True))
+# Sorted by USERNAME
+print('Per User INFO:', sorted(per_user_info.items(), key=operator.itemgetter(0)))
+print('Per User ERROR:', sorted(per_user_error.items(), key=operator.itemgetter(0)))
