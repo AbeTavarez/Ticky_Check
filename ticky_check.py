75c7819f876507f28d99555524231d11f23a1899
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-12 13:27:26 -0500

    generate report, first part

---
+++ ticky_check.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import re
+
+# Dict: Number of different error messages
+error_msg_number = {}
+# Dict: Count number of entries for each user
+# Splitting between INFO and ERROR
+user_entries_counter = {}
