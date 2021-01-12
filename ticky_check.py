4473b34c301c2b5b6624d9718f3ebf3030e81d20
Abraham E. Tavarez <abetavarez@icloud.com>
2021-01-12 16:54:51 -0500

    file iteration and dictionary population

--- ticky_check.py
+++ ticky_check.py
@@ -3,9 +3,40 @@
 import os
 import sys
 import re
+import operator
+import csv
 
 # Dict: Number of different error messages
-error_msg_number = {}
+errors = {}
 # Dict: Count number of entries for each user
-# Splitting between INFO and ERROR
-user_entries_counter = {}
+per_user = {}  # Splitting between INFO and ERROR
+
+# Sample Error
+# "May 27 11:45:40 ubuntu.local ticky: INFO: Created ticket [#1234] (username)"
+
+with open('syslog.log') as file:
+    # read each line
+    for line in file.readlines():
+        # regex search
+        info = re.search(r'INFO: ([\w]*)', line)
+        error = re.search(r'ERROR: ([\w]*)', line)
+
+        # Checks for successful match
+        if info is not None:
+            e = info.group(0)
+            if not e in per_user:
+                per_user[e] = 1
+            else:
+                per_user[e] += 1
+
+        # Checks if error got a match
+        if error is not None:
+            e = error.group(0)
+            if not e in errors:
+                errors[e] = 1
+            else:
+                errors[e] += 1
+file.close()
+
+print(errors)
+print(per_user)
